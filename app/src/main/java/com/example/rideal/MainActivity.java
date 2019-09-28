3b6d810d6734cde520d7fce5a0eb1414dd497176
Mustafa Eyceoz <me2680@columbia.edu>
2019-09-28 05:18:27 -0400

    Login Attempt

--- app/src/main/java/com/example/rideal/MainActivity.java
+++ app/src/main/java/com/example/rideal/MainActivity.java
@@ -1,29 +1,88 @@
 package com.example.rideal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import com.uber.sdk.android.core.UberSdk;
+import com.uber.sdk.android.core.auth.AccessTokenManager;
+import com.uber.sdk.android.core.auth.AuthenticationError;
+import com.uber.sdk.android.core.auth.LoginCallback;
+import com.uber.sdk.android.core.auth.LoginManager;
+import com.uber.sdk.core.auth.AccessToken;
 import com.uber.sdk.core.auth.Scope;
 import com.uber.sdk.rides.client.SessionConfiguration;
 
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+
+    LoginCallback loginCallback = new LoginCallback() {
+        @Override
+        public void onLoginCancel() {
+            // User canceled login
+        }
+
+        @Override
+        public void onLoginError(@NonNull AuthenticationError error) {
+            // Error occurred during login
+        }
+
+        @Override
+        public void onLoginSuccess(@NonNull AccessToken accessToken) {
+            // Successful login!  The AccessToken will have already been saved.
+        }
+
+        @Override
+        public void onAuthorizationCodeReceived(@NonNull String authorizationCode) {
+
+        }
+    };
+    AccessTokenManager accessTokenManager;
+    LoginManager loginManager;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        loginManager.onActivityResult(this, requestCode, resultCode, data);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SessionConfiguration config = new SessionConfiguration.Builder()
+                // mandatory
+                .setClientId("_uWysVxxI1pPuuOsaQSJZbgdk4VAzAzK")
+                // required for enhanced button features
+                // .setServerToken("<TOKEN>")
+                // required for implicit grant authentication
+                //   .setRedirectUri("<REDIRECT_URI>")
+                // optional: set sandbox as operating environment
+                .setEnvironment(SessionConfiguration.Environment.SANDBOX)
+                .setScopes(Arrays.asList(Scope.PROFILE, Scope.REQUEST))
+                .build();
+
+
+
         UberSdk.initialize(config);
         TextView hio = (TextView) findViewById(R.id.hello);
         hio.setText("UBER LINKED YAAAAAAAA");
 
         requestCheck();
+
+
+        accessTokenManager = new AccessTokenManager(this);
+        loginManager = new LoginManager(accessTokenManager, loginCallback);
+        loginManager.login(this);
     }
 
     private void requestCheck(){
@@ -50,20 +109,32 @@ public class MainActivity extends AppCompatActivity {
             }
         } else {
             // Permission has already been granted
+
         }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
 
-    SessionConfiguration config = new SessionConfiguration.Builder()
-            // mandatory
-            .setClientId("_uWysVxxI1pPuuOsaQSJZbgdk4VAzAzK")
-            // required for enhanced button features
-           // .setServerToken("<TOKEN>")
-            // required for implicit grant authentication
-         //   .setRedirectUri("<REDIRECT_URI>")
-            // optional: set sandbox as operating environment
-            .setEnvironment(SessionConfiguration.Environment.SANDBOX)
-            .build();
+                    requestCheck();
+                }
+                return;
+            }
 
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
 
 }
